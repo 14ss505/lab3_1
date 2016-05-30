57c10748416ffaaa2446607073b1ddd173122629
XINGXIAOYU <14302010053@fudan.edu.cn>
2016-05-30 11:33:20 +0800

    change Menu Constructor

--- TestSystem/src/MVC/view/MenuView.java
+++ TestSystem/src/MVC/view/MenuView.java
@@ -9,19 +9,7 @@ public class MenuView implements View{
 
     @Override
     public void display() {
-        setMenu("1) Create a new Survey\n"
-                + "2) Create a new Test\n"
-                + "3) Display Survey\n"
-                + "4) Display a Test \n"
-                + "5) Save a Survey\n"
-                + "6) Save a Test \n"
-                + "7) Modify a Survey\n"
-                + "8) Modify a Test\n"
-                + "9) Take a Survey\n"
-                + "10)Take a Test\n"
-                + "11)Look survey outcome\n"
-                + "12)Look test outcome\n"
-                + "13) Quit\n");
+        System.out.println(menu);
         setNext(sc.nextInt());
     }
 
